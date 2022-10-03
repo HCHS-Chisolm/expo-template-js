14be6faa95e808375ec723ee82cc4ac585e89503
Matthew Chisolm <mchisolm0@gmail.com>
2022-10-02 22:57:17 -0500

    removed unused deps and add prettier/eslint

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
